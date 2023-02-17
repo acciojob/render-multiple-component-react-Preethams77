1d1557fb22380de01051ab7a6b46896b2502fcbd
susan <susan.konala777@gmail.com>
2023-02-17 17:25:36 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import "./project.js";
 const App = () => {
   return (
     <div id="main"></div>
---
+++ src/project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const project = () => {
+  return (
+    <div>
+        <h1 attribute="data-ns-test" value ="project-name">Name</h1>
+        <h1  attribute="data-ns-test" value="project-description" >Description</h1>
+    </div>
+  )
+}
+
+export default project;
